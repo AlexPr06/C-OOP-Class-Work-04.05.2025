2420ffd6843f8038162a48df26ad6c3d41c8a6f6
Alex Prasov <freezay00828002@gmail.com>
2025-05-04 17:31:24 +0300

    Multithreaded project with pizzas has been completed!

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,5 @@
+//----------------------------- Project #1 ----------------------------------------------------------------------------------------------
+
 //#include <iostream>
 //#include <thread>
 //#include <mutex>
@@ -101,6 +103,144 @@
 //
 //}
 
+
+// ----------------------------- Project #2 ----------------------------------------------------------------------------------------------
+
+//#include <iostream>
+//#include <thread>
+//#include <mutex>
+//#include <condition_variable>
+//
+//#define RED "\033[31m"
+//#define GREEN "\033[32m"
+//#define YELLOW "\033[33m"
+//#define BLUE "\033[34m"
+//#define MAGENTA "\033[35m"
+//#define CYAN "\033[36m"
+//#define WHITE "\033[37m"
+//#define RESET "\033[0m"
+//
+//using namespace std;
+//
+//mutex mtx;
+//condition_variable cv;
+//bool production_completed = false;
+//bool packaging_completed;
+//
+//int currentBunsFactoryOne, currentBunsFactoryTwo = 0;
+//
+//const int EXPECTED_BUNS_FACTORY_1 = 50;
+//const int EXPECTED_BUNS_FACTORY_2 = 24;
+//
+//const int CONVEYOR_NUMBER = 5;
+//
+//void produceBunsFactory1(const string& factoryName)
+//{
+//	while (true)
+//	{
+//		unique_lock<mutex> lock(mtx);
+//		if (production_completed) break;
+//
+//		if (currentBunsFactoryOne < EXPECTED_BUNS_FACTORY_1)
+//		{
+//			currentBunsFactoryOne++;
+//			cout << GREEN << "Фабрика " << factoryName << " виготовила 1 булочку. Всього булочок фабрики " << factoryName << ": " << currentBunsFactoryOne << RESET << endl;
+//		}
+//		if (currentBunsFactoryOne >= EXPECTED_BUNS_FACTORY_1 && currentBunsFactoryTwo >= EXPECTED_BUNS_FACTORY_2)
+//		{
+//			production_completed = true;
+//			cv.notify_all();
+//			break;
+//		}
+//		lock.unlock();
+//		this_thread::sleep_for(chrono::milliseconds(100));
+//	}
+//}
+//
+//void produceBunsFactory2(const string& factoryName)
+//{
+//	while (true)
+//	{
+//		unique_lock<mutex> lock(mtx);
+//		if (production_completed) break;
+//
+//		if (currentBunsFactoryTwo < EXPECTED_BUNS_FACTORY_2)
+//		{
+//			currentBunsFactoryTwo++;
+//			cout << RED << "Фабрика " << factoryName << " виготовила 1 булочку. Всього булочок фабрики " << factoryName << ": " << currentBunsFactoryTwo << RESET << endl;
+//		}
+//		if (currentBunsFactoryOne >= EXPECTED_BUNS_FACTORY_1 && currentBunsFactoryTwo >= EXPECTED_BUNS_FACTORY_2)
+//		{
+//			production_completed = true;
+//			cv.notify_all();
+//			break;
+//		}
+//		lock.unlock();
+//		this_thread::sleep_for(chrono::milliseconds(100));
+//	}
+//}
+//
+//void conveyor(const int& conveyorNumber, const string& factoryNameOne, const string& factoryNameTwo)
+//{
+//	int lastPacked1 = 0;
+//	int lastPacked2 = 0;
+//
+//	while (true)
+//	{
+//		unique_lock<mutex> lock(mtx);
+//		if (production_completed && currentBunsFactoryOne - lastPacked1 < conveyorNumber && currentBunsFactoryTwo - lastPacked2 <= conveyorNumber) break;
+//
+//
+//		if (currentBunsFactoryOne - lastPacked1 >= conveyorNumber)
+//		{
+//			lastPacked1 += conveyorNumber;
+//			cout << YELLOW << "Конвеєр " << factoryNameOne << " упакував " << conveyorNumber << " булочок" << RESET << endl;
+//		}
+//		if (currentBunsFactoryTwo - lastPacked2 >= conveyorNumber)
+//		{
+//			lastPacked2 += conveyorNumber;
+//			cout << YELLOW << "Конвеєр " << factoryNameTwo << " упакував " << conveyorNumber << " булочок" << RESET << endl;
+//		}
+//		lock.unlock();
+//		this_thread::sleep_for(chrono::milliseconds(100));
+//	}
+//	unique_lock<mutex> lock(mtx);
+//	packaging_completed = true;
+//	cv.notify_all();
+//}
+//
+//void workDone()
+//{
+//	unique_lock<mutex> lock(mtx);
+//	cv.wait(lock, []()
+//	{
+//			return production_completed && packaging_completed;
+//	});
+//	cout << CYAN << "Виробництво завершено: "
+//		<< currentBunsFactoryOne << " булочок фабрики ROSHEN, "
+//		<< currentBunsFactoryTwo << " булочок фабрики MILKA." << RESET << endl;
+//}
+//
+//
+//int main()
+//{
+//	system("chcp 65001 > nul");
+//	thread t1(produceBunsFactory1, "ROSHEN");
+//	thread t2(produceBunsFactory2, "MILKA");
+//	thread t3(conveyor, CONVEYOR_NUMBER, "ROSHEN", "MILKA");
+//	thread t4(workDone);
+//
+//	t1.join();
+//	t2.join();
+//	t3.join();
+//	t4.join();
+//
+//
+//	return 0;
+//}
+
+// ----------------------------- Project #3 ----------------------------------------------------------------------------------------------
+
 #include <iostream>
 #include <thread>
 #include <mutex>
@@ -117,119 +257,159 @@
 
 using namespace std;
 
-mutex mtx;
+mutex mtxPizzasAndSauces;  // для піц і соусів
+mutex mtxBaking;           // для випічки
+mutex mtxPackaging;        // для пакування
+
+
+
 condition_variable cv;
-bool production_completed = false;
-bool packaging_completed;
+bool pizzas_completed = false;
+bool sauces_completed = false;
+bool baking_completed = false;
+bool packaging_completed = false;
 
-int currentBunsFactoryOne, currentBunsFactoryTwo = 0;
+int currentPizzas = 0, currentSauces = 0, bakedPizzas = 0, packagedPizzas = 0;
 
-const int EXPECTED_BUNS_FACTORY_1 = 50;
-const int EXPECTED_BUNS_FACTORY_2 = 24;
 
-const int CONVEYOR_NUMBER = 5;
+const int EXPECTED_PIZZAS = 50;
+const int EXPECTED_SAUCES = 50;
 
-void produceBunsFactory1(const string& factoryName)
+void producePizzas(const string& pizzaName)
 {
 	while (true)
 	{
-		unique_lock<mutex> lock(mtx);
-		if (production_completed) break;
-
-		if (currentBunsFactoryOne < EXPECTED_BUNS_FACTORY_1)
+		unique_lock<mutex> lockForPizzasAndSauces(mtxPizzasAndSauces);
+		if (pizzas_completed) break;
+		if (currentPizzas < EXPECTED_PIZZAS)
 		{
-			currentBunsFactoryOne++;
-			cout << GREEN << "Фабрика " << factoryName << " виготовила 1 булочку. Всього булочок фабрики " << factoryName << ": " << currentBunsFactoryOne << RESET << endl;
+			currentPizzas++;
+			cout << GREEN << "1 піца " << pizzaName << " виготовлена. Всього піц: " << currentPizzas << RESET << endl;
 		}
-		if (currentBunsFactoryOne >= EXPECTED_BUNS_FACTORY_1 && currentBunsFactoryTwo >= EXPECTED_BUNS_FACTORY_2)
+		if (currentPizzas >= EXPECTED_PIZZAS)
 		{
-			production_completed = true;
+			pizzas_completed = true;
 			cv.notify_all();
 			break;
 		}
-		lock.unlock();
+		lockForPizzasAndSauces.unlock();
 		this_thread::sleep_for(chrono::milliseconds(100));
 	}
 }
 
-void produceBunsFactory2(const string& factoryName)
+void produceSauces(const string& pizzaName)
 {
 	while (true)
 	{
-		unique_lock<mutex> lock(mtx);
-		if (production_completed) break;
-
-		if (currentBunsFactoryTwo < EXPECTED_BUNS_FACTORY_2)
+		unique_lock<mutex> lockForPizzasAndSauces(mtxPizzasAndSauces);
+		if (sauces_completed) break;
+		if (currentSauces < EXPECTED_SAUCES)
 		{
-			currentBunsFactoryTwo++;
-			cout << RED << "Фабрика " << factoryName << " виготовила 1 булочку. Всього булочок фабрики " << factoryName << ": " << currentBunsFactoryTwo << RESET << endl;
+			currentSauces++;
+			cout << RED << "1 соус для " << pizzaName << " виготовлений. Всього соусів: " << currentSauces << RESET << endl;
 		}
-		if (currentBunsFactoryOne >= EXPECTED_BUNS_FACTORY_1 && currentBunsFactoryTwo >= EXPECTED_BUNS_FACTORY_2)
+		if (currentSauces >= EXPECTED_SAUCES)
 		{
-			production_completed = true;
+			sauces_completed = true;
 			cv.notify_all();
 			break;
 		}
-		lock.unlock();
+		lockForPizzasAndSauces.unlock();
 		this_thread::sleep_for(chrono::milliseconds(100));
 	}
 }
 
-void conveyor(const int& conveyorNumber, const string& factoryNameOne, const string& factoryNameTwo)
-{
-	int lastPacked1 = 0;
-	int lastPacked2 = 0;
 
+void bakePizzas(const string& pizzaName)
+{
+	unique_lock<mutex> lockForPizzasAndSauces(mtxPizzasAndSauces);
+	cv.wait(lockForPizzasAndSauces, []()
+		{
+			return pizzas_completed && sauces_completed;
+		});
 	while (true)
 	{
-		unique_lock<mutex> lock(mtx);
-		if (production_completed && currentBunsFactoryOne - lastPacked1 < conveyorNumber && currentBunsFactoryTwo - lastPacked2 <= conveyorNumber) break;
-
-
-		if (currentBunsFactoryOne - lastPacked1 >= conveyorNumber)
+		unique_lock<mutex> lockForBaking(mtxBaking);
+		if (baking_completed) break;
+		if (bakedPizzas < EXPECTED_PIZZAS)
 		{
-			lastPacked1 += conveyorNumber;
-			cout << YELLOW << "Конвеєр " << factoryNameOne << " упакував " << conveyorNumber << " булочок" << RESET << endl;
+			bakedPizzas++;
+			cout << YELLOW << "1 піца " << pizzaName << " спечена. Всього спечених піц: " << bakedPizzas << RESET << endl;
 		}
-		if (currentBunsFactoryTwo - lastPacked2 >= conveyorNumber)
+		if (bakedPizzas >= EXPECTED_PIZZAS)
 		{
-			lastPacked2 += conveyorNumber;
-			cout << YELLOW << "Конвеєр " << factoryNameTwo << " упакував " << conveyorNumber << " булочок" << RESET << endl;
+			baking_completed = true;
+			cv.notify_all();
+			break;
 		}
-		lock.unlock();
+
+		lockForBaking.unlock();
 		this_thread::sleep_for(chrono::milliseconds(100));
 	}
-	unique_lock<mutex> lock(mtx);
-	packaging_completed = true;
-	cv.notify_all();
+}
+
+void packagePizzas()
+{
+	try {
+		unique_lock<mutex> lockForBaking(mtxBaking);
+		cv.wait(lockForBaking, []() {
+			return baking_completed;
+			});
+
+		while (true)
+		{
+			unique_lock<mutex> lockForPackaging(mtxPackaging);
+			if (packaging_completed) break;
+
+			if (packagedPizzas < EXPECTED_PIZZAS)
+			{
+				packagedPizzas++;
+				cout << BLUE << "1 піца упакована. Всього упакованих піц: " << packagedPizzas << RESET << endl;
+			}
+			if (packagedPizzas >= EXPECTED_PIZZAS)
+			{
+				packaging_completed = true;
+				cv.notify_all();
+				break;
+			}
+			lockForPackaging.unlock();
+			this_thread::sleep_for(chrono::milliseconds(100));
+		}
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Помилка: " << e.what() << std::endl;
+	}
 }
 
 void workDone()
 {
-	unique_lock<mutex> lock(mtx);
-	cv.wait(lock, []()
-	{
-			return production_completed && packaging_completed;
-	});
-	cout << CYAN << "Виробництво завершено: "
-		<< currentBunsFactoryOne << " булочок фабрики ROSHEN, "
-		<< currentBunsFactoryTwo << " булочок фабрики MILKA." << RESET << endl;
+	try {
+		unique_lock<mutex> lockForPackaging(mtxPackaging);
+		cv.wait(lockForPackaging, []()
+			{
+				return packaging_completed;
+			});
+		cout << CYAN << "Виробництво завершено!" << RESET;
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Помилка: " << e.what() << std::endl;
+	}
 }
 
-
-int main()
-{
+int main() {
 	system("chcp 65001 > nul");
-	thread t1(produceBunsFactory1, "ROSHEN");
-	thread t2(produceBunsFactory2, "MILKA");
-	thread t3(conveyor, CONVEYOR_NUMBER, "ROSHEN", "MILKA");
-	thread t4(workDone);
 
-	t1.join();
-	t2.join();
-	t3.join();
-	t4.join();
+	thread pizzas(producePizzas, "Pepperoni ");
+	thread sauces(produceSauces, "Pepperoni ");
+	thread baking(bakePizzas, "Pepperoni ");
+	thread packaging(packagePizzas);
+	thread pizzasReadyForDelivering(workDone);
 
-
+	pizzas.join();
+	sauces.join();
+	baking.join();
+	packaging.join();
+	pizzasReadyForDelivering.join();
+	
 	return 0;
 }
